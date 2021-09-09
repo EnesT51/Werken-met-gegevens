e5cf31535fdda4bb3999a82d7862110d9136ba16
EnesT51 <99040669@mydavinci.nl>
2021-09-09 20:53:19 +0200

    Door mij gemaakte py programma

---
+++ map calculatie/feestlunch.py
@@ -0,0 +1,5 @@
+croissantjes = 17 * 0.39
+stokbrood = 2 * 2.78
+kortingsbonnen = 1.50
+
+print(croissantjes + stokbrood - kortingsbonnen) 
---
+++ map calculatie/speelhaldag.py
@@ -0,0 +1,4 @@
+DrieVrieden  = 3 * 7.45 
+Gameset = 27 * 0.37
+
+print (DrieVrieden + Gameset) 
